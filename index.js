18316ddfd7d83774d73c2373f37d69bb1230e914
chihebbhy <chiheb147bhy@gmail.com>
2026-03-20 00:24:39 +0200

    fixed hexagons

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const parent = document.getElementById("map");
-for (let j = 0; j < 10; j++) {
+for (let j = 0; j < 30; j++) {
     let row = document.createElement("div");
     row.classList.add('row',`row${j}`);
     parent.appendChild(row);
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < 30; i++) {
         let hex = document.createElement("div");
-        hex.classList.add('hex', `hex${i}`);
+        hex.classList.add('hex', `hex${j}${i}`);
+        hex.addEventListener('click', Click);
+
         row.appendChild(hex);
         let top = document.createElement("div");
         let mid = document.createElement("div");
         let bot = document.createElement("div");
-        top.classList.add('top', `top${i}`);
-        mid.classList.add('mid', `mid${i}`);
-        bot.classList.add('bot', `bot${i}`);
+        top.classList.add('top', `top${j}${i}`);
+        mid.classList.add('mid', `mid${j}${i}`);
+        bot.classList.add('bot', `bot${j}${i}`);
         hex.appendChild(top);
         hex.appendChild(mid);
         hex.appendChild(bot);
     }
+}
+
+function Click(e){
+    alert(e.target.classList);
 }
