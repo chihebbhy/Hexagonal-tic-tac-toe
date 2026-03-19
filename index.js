0312f9c26513cd351567bb9bd0e2a5968c89a9cf
chihebbhy <chiheb147bhy@gmail.com>
2026-03-19 22:54:51 +0200

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const parent = document.getElementById("map");
-for (let i = 0; i < 2; i++) {
-    let top = document.createElement("div");
-    let mid = document.createElement("div");
-    let bot = document.createElement("div");
-    top.classList.add('top','${i}');
-    mid.classList.add('mid','${i}');
-    bot.classList.add('bot','${i}');
-    parent.appendChild(top);
-    parent.appendChild(mid);
-    parent.appendChild(bot);
-}   
+for (let j = 0; j < 10; j++) {
+    let row = document.createElement("div");
+    row.classList.add('row',`row${j}`);
+    parent.appendChild(row);
+    for (let i = 0; i < 11; i++) {
+        let hex = document.createElement("div");
+        hex.classList.add('hex', `hex${i}`);
+        row.appendChild(hex);
+        let top = document.createElement("div");
+        let mid = document.createElement("div");
+        let bot = document.createElement("div");
+        top.classList.add('top', `top${i}`);
+        mid.classList.add('mid', `mid${i}`);
+        bot.classList.add('bot', `bot${i}`);
+        hex.appendChild(top);
+        hex.appendChild(mid);
+        hex.appendChild(bot);
+    }
+}
